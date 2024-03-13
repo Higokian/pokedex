f0b3b30674db8407c0fc53b0ff917343c212c919
Jason <jason_ritter@outlook.com>
2024-03-13 02:09:29 -0500

    Cleaned up initial create-react-up

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-paragraph">This is a test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
