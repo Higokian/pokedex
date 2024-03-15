ac485dc787652449baba5afdaea625a1be2b6eb2
Jason <jason_ritter@outlook.com>
2024-03-14 19:22:51 -0500

    Created pokedex component/layout

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,2 +1,28 @@
 // Provided array of objects describing different pokemon
 // --> Renders series of Pokecards
+import React, {Component} from 'react';
+import Pokecard from './Pokecard';
+import '../css/Pokedex.css'
+
+class Pokedex extends Component {
+    render() {
+        return (
+            <div className="Pokedex">
+                <div className="Pokedex-row">
+                    <Pokecard />
+                    <Pokecard />
+                    <Pokecard />
+                    <Pokecard />
+                </div>
+                <div className="Pokedex-row">
+                    <Pokecard />
+                    <Pokecard />
+                    <Pokecard />
+                    <Pokecard />
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Pokedex;
