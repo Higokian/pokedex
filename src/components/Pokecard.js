707edf7671c7e0c74a52b280c2fe98deb351a02b
Jason <jason_ritter@outlook.com>
2024-03-15 12:19:18 -0500

    Created API image paths using props

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -6,17 +6,19 @@ import React, {Component} from 'react';
 import '../css/Pokecard.css';
 import '../css/index.css';
 
-// let POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+const POKE_API = 
+'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component {
     render() { 
+        let imgSrc = `${POKE_API}${this.props.id}.png`
         return (
             <div className="Pokecard">
-                <h2 className="Pokecard-name">Charmander</h2>
-                <img className="Pokecard-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" />
+                <h2 className="Pokecard-name">{this.props.name}</h2>
+                <img className="Pokecard-img" src={imgSrc} />
                 <div className="Pokecard-info">
-                    <p>Type: fire</p>
-                    <p>EXP: 62</p>
+                    <p>Type: {this.props.type}</p>
+                    <p>EXP: {this.props.exp}</p>
                 </div>
             </div>
         );
