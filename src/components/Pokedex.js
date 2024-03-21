71569d69e05adc8ca0145b29465f09083d1b234e
Jason <jason_ritter@outlook.com>
2024-03-20 19:01:11 -0500

    Created a function to shuffle all cards

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -18,13 +18,24 @@ class Pokedex extends Component {
             {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
         ]
     }
+
     render() {
         
+        function shuffleCards(arr) {
+            for (let i = arr.length - 1; i > 0; i--) {
+                // Create random index between 0 and i
+                let randomCard = Math.floor(Math.random() * (i + 1));
+
+                // Swap random cards
+                [arr[i], arr[randomCard]] = [arr[randomCard], arr[i]];
+            }
+            return arr;
+        }
         
         return (
             <div className="Pokedex">
                 <div className="Pokedex-row">
-                    {this.props.pokemon.map((p) => (
+                    {shuffleCards(this.props.pokemon).map((p) => (
                         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
                     ))}
                 </div>
---
+++ src/components/Pokegame.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Pokegame extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
